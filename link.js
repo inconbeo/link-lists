8d379767645090a4f810e853e04b494cda48e0e9
Eddie Yao <adpoyao@gmail.com>
2017-12-14 09:14:55 -0800

    added remove and retrieve

--- link.js
+++ link.js
@@ -1,33 +1,69 @@
+'use strict';
+
 class LinkList {
-    constructor () {
-        this.length = 0;
-        this.head = null;
+  constructor () {
+    this.length = 0;
+    this.head = null;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index > this.length) {
+      throw new Error('Index Error');
+    }
+    const newNode = {value};
+    if (index === 0) {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    else {
+      let before = this._find(index-1);
+      newNode.next = before.next;
+      before.next = newNode;
+    }
+    this.length++;
+  }
+
+  _find(index) {
+    let node = this.head;
+    for (let i = 0; i < index; i++) {
+      node = node.next;
+    }
+    return node;
+  }
+
+  retrieve(index){
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index Error');
+    }
+    const node = this._find(index);
+    return node.value;
+  }
+
+  remove(index){
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index Error');
     }
 
-    insert(index, value) {
-        if (index < 0 || index >= this.length) {
-            throw new Error('Index Error')
-        }
-        const newNode = {value};
-        if (index === 0) {
-            newNode.next = this.head;
-            this.head = newNode;
-        }
-        else {
-            let before = this._find(index-1);
-            newNode.next = before.next;
-            before.next = newNode;
-        }
-        this.length++;
+    //if HEAD
+    if(index === 0){
+      this.head = this.head.next;
     }
 
-    _find(index) {
-        let node = this.head;
-        for (let i = 0; i < index; i++) {
-            node = node.next;
-        }
-        return node;
+    //if not HEAD
+    else {
+      let before = this._find(index - 1);
+      before.next = before.next.next;
     }
-
-    
+    this.length--;
+  }
 } 
+
+const linkList = new LinkList();
+
+linkList.insert(0, 'Eddie');
+linkList.insert(1, 'Quang');
+console.log(linkList);
+
+console.log('retrieve', linkList.retrieve(1));
+console.log('remove', linkList.remove(0));
+console.log('retireve', linkList.retrieve(0));
